f80932cfe10258401a62dbab01e6b3943da373aa
Paweł Ficek <pawel.ficek@gmail.com>
2019-01-13 15:38:10 +0100

    Refactored example lua script

--- script.lua
+++ script.lua
@@ -1,34 +1,39 @@
 number = 0
 
+--ai_turn is a function used to calculate AI move. Don't modify its name
+
 function ai_turn(cards, tableCards, history)
     command = ""
     print "TEST AI FUNCTION!"
-    print("NUMBER " .. number)
+    print("NUMBER_OF_AI_RUNS " .. number)
     number = number + 1
-    numberOfCards = cards:numberOfCards()
-    for i=0, numberOfCards - 1 do
+    print_cards(cards, numberOfCards)
+
+    if history:lengthOfHistory() == 0 then
+        throw_starting_cards(cards)
+    else
+        command = "PASS TURN"
+    end
+    return command
+end
+
+function throw_starting_cards(cards, numberOfCards)
+    command = "THROW"
+
+    for i=0, cards:numberOfCards() - 1 do
+        card = cards:at(i)
+        if (card.value == 7) then
+            command = command .. " " .. i
+        end
+    end 
+end
+
+function print_cards(cards)
+    for i=0, cards:numberOfCards() - 1 do
         card = cards:at(i)
         print(card.value)
         print(card.color)
         print(card.selected)
     end
-
-    if history:lengthOfHistory() == 0 then
-        print("LENGTH " .. history:lengthOfHistory())
-        command = "THROW"
-
-        for i=0, numberOfCards - 1 do
-            card = cards:at(i)
-            if (card.value == 7) then
-                command = command .. " " .. i
-            end
-            print(card.value)
-            print(card.color)
-            print(card.selected)
-        end 
-    else
-        command = "PASS TURN" --THROW 0 1 2 3
-    end
-    return command
 end
 
