bad79a0ebfff39ad714eba77e06f80f4b2b89f7a
Paweł Ficek <pawel.ficek@gmail.com>
2019-01-13 18:24:51 +0100

    Lua example AI can throw cards higher than last on table

--- script.lua
+++ script.lua
@@ -12,7 +12,17 @@ function ai_turn(cards, tableCards, history)
     if history:lengthOfHistory() == 0 then
         throw_starting_cards(cards)
     else
-        command = "PASS TURN"
+        print("CARDS ON TABLE: ")
+        print_cards(tableCards)
+        cards_higher_than_on_table = find_cards_higher_than_given_value(cards, tableCards:numberOfCards(), tableCards:at(0).value)
+        if #cards_higher_than_on_table > 0 then
+            command = "THROW"
+            for i=1, #cards_higher_than_on_table[1] do
+                command = command .. " " .. cards_higher_than_on_table[1][i]
+            end
+        else
+            command = "PASS TURN"
+        end
     end
     return command
 end
@@ -25,7 +35,7 @@ function throw_starting_cards(cards, numberOfCards)
         if (card.value == 7) then
             command = command .. " " .. i
         end
-    end 
+    end
 end
 
 function print_cards(cards)
@@ -35,5 +45,43 @@ function print_cards(cards)
         print(card.color)
         print(card.selected)
     end
+end
+
+function count_cards_of_given_value(cards, value)
+    noOfCards = 0
+    for i=0, cards:numberOfCards() - 1 do
+        card = cards:at(i)
+        if (card.value == value) then
+            noOfCards = noOfCards + 1
+        end
+    end
+    return noOfCards
+end
+
+function get_indexes_of_cards_with_given_value(cards, value)
+    indexes = {}
+    for i=0, cards:numberOfCards() - 1 do
+        card = cards:at(i)
+        if (card.value == value) then
+            indexes[#indexes + 1] = i
+            print "SOMETHING ADDED!"
+        end
+    end
+    return indexes
+end
+
+function find_cards_higher_than_given_value(cards, numberOfCards, value)
+    return_array = {}
+
+    for i=0, cards:numberOfCards() - 1 do
+        if cards:at(i).value > value then
+            noOfCards = count_cards_of_given_value(cards, cards:at(i).value)
+            if noOfCards == numberOfCards then
+                return_array[#return_array + 1] = get_indexes_of_cards_with_given_value(cards, cards:at(i).value)
+            end
+        end
+    end
+
+    return return_array
 end
 
